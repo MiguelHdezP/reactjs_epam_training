01ab5fbf604c33a9640c0b5054ec450210778f33
Miguel Hernandez <miguel_hernandez1@epam.com>
2021-10-24 20:13:01 -0500

    removing simple button

--- src/components/shared/buttons/simple-button/SimpleButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function SimpleButton({ text = "", classStyle = "" }) {
-  return (
-    <button type="button" className={classStyle}>
-      {text}
-    </button>
-  );
-}
--- src/components/shared/multi-select/MultiSelect.jsx
+++ src/components/shared/multi-select/MultiSelect.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./MultiSelect.scss";
-import SimpleButton from "../buttons/simple-button/SimpleButton"
+import ActionButton from "../buttons/action-button/ActionButton";
 import DropdownLists from "../lists/DropdownLists";
 
 export default function MultiSelect({ options }) {
   return (
     <div className="multiselect-dropdown" id="valueItemDrop">
-      <SimpleButton text="Select Genre" classStyle={"multi-select-style"} />
+      <ActionButton text="Select Genre" classStyle={"multi-select-style"} />
       <DropdownLists lists={options} />
     </div>
   );
